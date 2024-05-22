b69be00d805fd2f3e60f0d6c0a18812742a7c66a
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2024-05-22 16:56:33 +0200

    Update EAIDataBaseManager.cs

--- MOD/EAIDataBaseManager.cs
+++ MOD/EAIDataBaseManager.cs
@@ -42,7 +42,7 @@ internal static class EAIDataBaseManager
 			DataBaseVersion = DataBaseVersion,
 			AssetsDataBase =  ValidateAssetsDataBase,
         };
-		string directoryPath = new FileInfo(pathToAssetsDatabase).Directory.FullName;
+        string directoryPath = Path.GetDirectoryName(pathToAssetsDatabase);
         if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
         File.WriteAllText(pathToAssetsDatabase, Encoder.Encode(dataBase, EncodeOptions.None));
 	}
