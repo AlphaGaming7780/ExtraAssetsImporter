2398691dcd951e75aaf21cee6b9135412d0ac4a5
Triton Supreme <69048433+AlphaGaming7780@users.noreply.github.com>
2025-11-25 19:15:10 +0100

    Update RenderedAreaJson.cs

--- MOD/AssetImporter/JSONs/Components/RenderedAreaJson.cs
+++ MOD/AssetImporter/JSONs/Components/RenderedAreaJson.cs
@@ -1,8 +1,52 @@
-﻿namespace ExtraAssetsImporter.AssetImporter.JSONs.Components
+﻿using Colossal.IO.AssetDatabase;
+using Game.Rendering;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace ExtraAssetsImporter.AssetImporter.JSONs.Components
 {
     internal class RenderedAreaJson : ComponentJson
     {
+
         public float Roundness = 0.5f;
         public float LodBias = 0;
+
+        public float m_Roundness = 0.5f;
+
+        public float m_LodBias;
+
+        public int m_RendererPriority;
+
+        public DecalLayers m_DecalLayerMask = DecalLayers.Terrain;
+
+        public Color m_BaseColor = Color.white;
+
+        public AssetReference<TextureAsset> m_BaseColorMap;
+
+        public AssetReference<TextureAsset> m_NormalMap;
+
+        public AssetReference<TextureAsset> m_MaskMap;
+
+        public float m_Metallic = 1f;
+
+        public float m_Smoothness = 1f;
+
+        public float m_NormalOpacity = 1f;
+
+        public float m_MetallicOpacity = 1f;
+
+        public float m_NormalAlphaSource;
+
+        public float m_MetallicAlphaSource;
+
+        public float m_UVScale = 0.2f;
+
+        public float m_EdgeNormal = 0.5f;
+
+        public float2 m_EdgeFadeRange = new float2(0.75f, 0.25f);
+
+        public float2 m_EdgeNormalRange = new float2(0.7f, 0.5f);
+
+        public float2 m_EdgeNoise = new float2(0.5f, 0.5f);
     }
 }
